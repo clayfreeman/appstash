276cb93057eb5c4e098d4c724576605028309a36
Clay Freeman <git@clayfreeman.com>
2016-07-28 19:05:17 -0500

    change package directory to `debs`

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git add -A packages
-dpkg-scanpackages packages override 2> /dev/null | tee Packages | \
+git add -A debs
+dpkg-scanpackages debs override 2> /dev/null | tee Packages | \
   gzip -9c > Packages.gz

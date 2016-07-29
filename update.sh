1967003ac3d1193554abfa4bc3d52a5a95da87ed
Clay Freeman <git@clayfreeman.com>
2016-07-28 19:24:56 -0500

    update update.sh

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@ git add -A deb
 dpkg-scanpackages deb override 2> /dev/null | tee Packages | \
   gzip -9c > Packages.gz
 git commit -am 'new release'
+git push -u origin gh-pages
 git checkout master

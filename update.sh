39ab604dd08ed2aafd9e6f63ef1f273a4c641ede
Clay Freeman <git@clayfreeman.com>
2016-07-28 19:08:32 -0500

    rename to `deb`

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git add -A debs
-dpkg-scanpackages debs override 2> /dev/null | tee Packages | \
+git add -A deb
+dpkg-scanpackages deb override 2> /dev/null | tee Packages | \
   gzip -9c > Packages.gz
